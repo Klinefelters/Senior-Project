a64a431a0af2954f44e1c128c19b59765d744dd3
klinefelters <stevenjk03@gmail.com>
2024-03-25 11:58:25 -0400

    Installed Chakra-UI and set up boilerplate code

--- senior-sage/src/main.jsx
+++ senior-sage/src/main.jsx
@@ -1,10 +1,40 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./styles.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import {ChakraProvider, extendTheme} from '@chakra-ui/react';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const colors = {
+  brand: {
+    dark_blue: '#0A2240',
+    mid_blue: '#004B98',
+    light_blue: '#3DB5E6',
+    grey: '#C8C8C8',
+    green: '#61BF1A',
+    red: '#E1261C',
+  },
+}
+
+
+const fonts = {
+  body: 'Tahoma',
+  heading: 'Courier New'
+}
+
+const theme = extendTheme({
+  config: {
+    initialColorMode: 'dark',
+    useSystemColorMode: false,
+  },
+  colors, 
+  fonts,
+});
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
-);
+)
