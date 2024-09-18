0e5cf2914cb24fc07b2e42fdf7ce9a0a8f0a39eb
Steven Klinefelter <134639644+Klinefelters@users.noreply.github.com>
2024-09-17 20:31:07 -0400

    Model loading

--- senior-sage/src/components/MicrophoneButton.jsx
+++ senior-sage/src/components/MicrophoneButton.jsx
@@ -1,7 +1,42 @@
+import { useState, useEffect } from 'react';
 import { IconButton} from '@chakra-ui/react';
 import { FaMicrophone } from "react-icons/fa";
+import { createModel, KaldiRecognizer, Model } from "vosk-browser";
 
 export default function MicrophoneButton({setText}) {
+    const [recognizer, setRecognizer] = useState(null);
+    const [loadedModel, setLoadedModel] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    const loadModel = async (path) => {
+        setLoading(true);
+
+        const model = await createModel( "/models/" + path);
+        setLoadedModel({ model, path });
+
+        const recognizer = new model.KaldiRecognizer(48000);
+        recognizer.setWords(true);
+        recognizer.on("result", (message) => {
+            const result= message.result;
+            setUtterances((utt) => [...utt, result]);
+        });
+
+        recognizer.on("partialresult", (message) => {
+            setPartial(message.result.partial);
+        });
+
+        setRecognizer(() => {
+            setLoading(false);
+            // setReady(true);
+            return recognizer;
+        });
+        console.log("Model loaded");
+    }
+
+    useEffect(() => {
+        loadModel('vosk-model-small-en-us-0.15.tar.gz')
+    }, []);
+
     const size = "5vw";
     return (
         <IconButton icon={<FaMicrophone/>} h={size} w={size} />
