4f1b4c9bc5bb4dacfb70dd305737350c6d66702c
klinefelters <stevenjk03@gmail.com>
2024-09-20 14:12:12 -0400

    Create a speech Synthesis Service

---
+++ senior-sage/src/services/speechSynthesisService.js
@@ -0,0 +1,12 @@
+export default class SpeechSynthesisService {
+	static async speak(text, voice = 0, speed = 1, volume = 1, pitch = 1) {
+		let speech = window.speechSynthesis;
+		let utterance = new SpeechSynthesisUtterance(text);
+		utterance.voice = speechSynthesis.getVoices()[voice];
+		utterance.rate = speed;
+		utterance.pitch = pitch;
+		utterance.volume = volume;
+		speech.cancel();
+		return speech.speak(utterance);
+	}
+}
