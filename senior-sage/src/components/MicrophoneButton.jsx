d7b46b808c3a84e6aa7401e0afe1be1154b8e9fa
Steven Klinefelter <134639644+Klinefelters@users.noreply.github.com>
2024-09-19 20:05:29 -0400

    Add state variables and update text display in MicrophoneButton component

--- senior-sage/src/components/MicrophoneButton.jsx
+++ senior-sage/src/components/MicrophoneButton.jsx
@@ -4,9 +4,13 @@ import { FaMicrophone } from "react-icons/fa";
 import { createModel, KaldiRecognizer, Model } from "vosk-browser";
 
 export default function MicrophoneButton({setText}) {
+    const [utterances, setUtterances] = useState([]);
+    const [partial, setPartial] = useState("");
     const [recognizer, setRecognizer] = useState(null);
     const [loadedModel, setLoadedModel] = useState(null);
     const [loading, setLoading] = useState(false);
+    const [ready, setReady] = useState(false);
+    const size = "5vw";
 
     const loadModel = async (path) => {
         setLoading(true);
@@ -19,15 +23,17 @@ export default function MicrophoneButton({setText}) {
         recognizer.on("result", (message) => {
             const result= message.result;
             setUtterances((utt) => [...utt, result]);
+            setText((utt) => [...utt, result]);
         });
 
         recognizer.on("partialresult", (message) => {
             setPartial(message.result.partial);
+            setText(message.result.partial);
         });
 
         setRecognizer(() => {
             setLoading(false);
-            // setReady(true);
+            setReady(true);
             return recognizer;
         });
         console.log("Model loaded");
@@ -37,7 +43,6 @@ export default function MicrophoneButton({setText}) {
         loadModel('vosk-model-small-en-us-0.15.tar.gz')
     }, []);
 
-    const size = "5vw";
     return (
         <IconButton icon={<FaMicrophone/>} h={size} w={size} />
     );
