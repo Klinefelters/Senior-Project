b16a3b5ba5687fafbab7c71350f43140299ceca8
klinefelters <stevenjk03@gmail.com>
2024-09-20 13:59:33 -0400

    Remove InputBot Code from last semester Hardware Interactions POC

--- senior-sage/src-tauri/src/main.rs
+++ senior-sage/src-tauri/src/main.rs
@@ -1,22 +1,9 @@
-use inputbot::KeybdKey::SpaceKey;
-use std::thread;
-use tauri::Manager;
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    let app = tauri::Builder::default()
+    tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![])
-        .build(tauri::generate_context!())
-        .expect("error while building tauri application");
-
-    let app_handle = app.handle();
-
-    SpaceKey.bind(move || {
-        app_handle.emit_all("spacebar", true).unwrap();
-    });
-
-    thread::spawn(|| {
-        inputbot::handle_input_events();
-    });
-
-    app.run(|_, _| {});
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
