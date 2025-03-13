c4a6bf19b49171fd6823cb65096f43110ccbda39
jculk <j.culkin319@gmail.com>
2025-03-13 12:33:04 -0400

    Small Changes for Linux

--- senior-sage/src-tauri/build.rs
+++ senior-sage/src-tauri/build.rs
@@ -1,4 +1,5 @@
 fn main() {
+<<<<<<< HEAD
     #[cfg(all(target_os = "linux", target_arch = "aarch64"))]
     {
         let lib_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap())
@@ -8,3 +9,9 @@ fn main() {
 
     tauri_build::build();
 }
+=======
+    // println!("cargo:rustc-link-search=libvosk");
+    println!("cargo:rustc-link-lib=libvosk");
+    tauri_build::build()
+}
+>>>>>>> 4471958 (Small Changes for Linux)
