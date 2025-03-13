bb4756cf2922c2c253cb44897cea848e217770cc
jculk <j.culkin319@gmail.com>
2025-03-13 16:43:29 -0400

    TTS Integrated

--- senior-sage/src-tauri/src/main.rs
+++ senior-sage/src-tauri/src/main.rs
@@ -1,53 +1,25 @@
-// use inputbot::KeybdKey::CapsLockKey;
-// use pv_recorder::PvRecorderBuilder;
-// use std::{thread, thread::sleep, time::Duration};
-// use tauri::Manager;
+use std::process::Command;
 
-// mod vosk;
+#[tauri::command]
+async fn speak_text(input_text: String) -> String {
+    let command = format!("echo '{}' |   ./piper/piper --model ./piper/en_US-ryan-high.onnx --output-raw |   aplay -r 22050 -f S16_LE -t raw -", input_text);
+    println!("Command: {}", command); // Print the command for debugging
+    let output1 = Command::new("sh")
+        .arg("-c")
+        .arg(command)
+        .output()
+        .expect("Failed to execute command");
+    println!("{:?}", &output1); // Use {:?} for debug formatting
+    println!("Command Output:\n{}", String::from_utf8_lossy(&output1.stdout));  
+    println!("Done");
+    return "done".to_string();
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let app = tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![])
+        .invoke_handler(tauri::generate_handler![speak_text])
         .build(tauri::generate_context!())
         .expect("error while building tauri application");
-
-    // let app_handle = app.handle();
-
-    // vosk::init_vosk();
-
-    // let recorder = PvRecorderBuilder::new(512).init()?;
-
-    // CapsLockKey.bind(move || {
-    //     while CapsLockKey.is_toggled() {
-
-    //         if !recorder.is_recording() {
-    //             recorder.start().unwrap();
-    //         }
-
-    //         let frame = recorder.read().unwrap();
-
-    //         if let Some(transcription) = vosk::recognize(&frame, true) {
-    //             if transcription.is_empty() {
-    //                 continue;
-    //             }
-    //             println!("{}", transcription);
-    //             app_handle
-    //                 .emit_all("transcription", transcription)
-    //                 .expect("failed to emit transcription");
-    //         }
-
-            
-    //         sleep(Duration::from_millis(30));
-    //     }
-    //     if recorder.is_recording(){
-    //         recorder.stop().unwrap();
-    //     }
-        
-    // });
-
-    // thread::spawn(|| {
-    //     inputbot::handle_input_events();
-    // });
 
     app.run(|_, _| {});
     Ok(())
