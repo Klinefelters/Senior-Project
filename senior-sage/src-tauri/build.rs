d8014de4952d106ada5a7b99ba53dc6a13c54e2b
joshschmitt35 <157548551+joshschmitt35@users.noreply.github.com>
2025-03-13 21:31:00 -0400

    Adding a Perma Path for libvosk.so

--- senior-sage/src-tauri/build.rs
+++ senior-sage/src-tauri/build.rs
@@ -1,4 +1,5 @@
 fn main() {
+    let lib_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap()).join("/home/orangepi/Senior-Project/senior-sage/src-tauri/libvosk");
     println!("cargo:rustc-link-search=libvosk");
     tauri_build::build()
 }
