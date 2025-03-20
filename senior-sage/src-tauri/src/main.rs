316497cb02187bb412375c2ee9d879ad701a1aad
klinefelters <klinefelters@etown.edu>
2025-03-20 15:34:35 -0400

    Fixed

--- senior-sage/src-tauri/src/main.rs
+++ senior-sage/src-tauri/src/main.rs
@@ -12,6 +12,59 @@ use dasp::{sample::ToSample, Sample};
 mod vosk;
 
 #[tauri::command]
+async fn listen_and_transcribe(app_handle: tauri::AppHandle) -> String {
+    let audio_input_device = cpal::default_host()
+        .default_input_device()
+        .expect("No input device connected");
+
+    let config = audio_input_device
+        .default_input_config()
+        .expect("Failed to load default input config");
+    let channels = config.channels();
+
+
+    let err_fn = move |err| {
+        eprintln!("an error occurred on stream: {}", err);
+    };
+    println!("Starting transcription");
+
+    let stream = match config.sample_format() {
+        SampleFormat::I8 => audio_input_device.build_input_stream(
+            &config.into(),
+            move |data: &[i8], _| recognize(app_handle.clone(), data, channels),
+            err_fn,
+            None,
+        ),
+        SampleFormat::I16 => audio_input_device.build_input_stream(
+            &config.into(),
+            move |data: &[i16], _| recognize(app_handle.clone(), data, channels),
+            err_fn,
+            None,
+        ),
+        SampleFormat::I32 => audio_input_device.build_input_stream(
+            &config.into(),
+            move |data: &[i32], _| recognize(app_handle.clone(), data, channels),
+            err_fn,
+            None,
+        ),
+        SampleFormat::F32 => audio_input_device.build_input_stream(
+            &config.into(),
+            move |data: &[f32], _| recognize(app_handle.clone(), data, channels),
+            err_fn,
+            None,
+        ),
+        sample_format => panic!("Unsupported sample format '{sample_format}'"),
+    }
+    .expect("Could not build stream");
+
+    stream.play().expect("Could not play stream");
+    sleep(Duration::from_secs(5));
+    drop(stream);
+    let result = "Transcription stopped".to_string();
+    result
+}
+
+#[tauri::command]
 async fn speak_text(input_text: String) -> String {
     let command = format!("echo '{}' |   ./piper/piper --model ./piper/en_US-ryan-high.onnx --output-raw |   aplay -r 22050 -f S16_LE -t raw -", input_text);
     println!("Command: {}", command); // Print the command for debugging
@@ -37,7 +90,7 @@ fn main() {
     vosk::init_vosk(config.sample_rate().0 as f32);
     tauri::Builder::default()
       // This is where you pass in your commands
-      .invoke_handler(tauri::generate_handler![listen_and_transcribe])
+      .invoke_handler(tauri::generate_handler![listen_and_transcribe, speak_text])
       .run(tauri::generate_context!())
       .expect("failed to run app");
 }
