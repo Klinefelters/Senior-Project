a226e0ff5b2b008e67fddf9b0559307a2d8a8986
klinefelters <klinefelters@etown.edu>
2025-03-20 13:43:02 -0400

    changed to cpal, working kinda

--- senior-sage/src-tauri/build.rs
+++ senior-sage/src-tauri/build.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let lib_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap()).join("/home/orangepi/Senior-Project/senior-sage/src-tauri/libvosk");
+    //let lib_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap()).join("/home/orangepi/Senior-Project/senior-sage/src-tauri/libvosk");
     println!("cargo:rustc-link-search=libvosk");
     tauri_build::build()
 }
--- senior-sage/src-tauri/src/main.rs
+++ senior-sage/src-tauri/src/main.rs
@@ -1,52 +1,113 @@
-use pv_recorder::PvRecorderBuilder;
+
 use tauri::Manager;
 use std::thread::sleep;
 use std::time::Duration;
+
+use cpal::{
+    traits::{DeviceTrait, HostTrait, StreamTrait},
+    ChannelCount, SampleFormat,
+};
+use dasp::{sample::ToSample, Sample};
 mod vosk;
 
 #[tauri::command]
 async fn listen_and_transcribe(app_handle: tauri::AppHandle) -> String {
-    let recorder = PvRecorderBuilder::new(512).init().expect("failed to initialize recorder");
-    let mut transcription = "".to_string();
+    let audio_input_device = cpal::default_host()
+        .default_input_device()
+        .expect("No input device connected");
+
+    let config = audio_input_device
+        .default_input_config()
+        .expect("Failed to load default input config");
+    let channels = config.channels();
+
+
+    let err_fn = move |err| {
+        eprintln!("an error occurred on stream: {}", err);
+    };
     println!("Starting transcription");
 
-    
-    loop {
-        if !recorder.is_recording() {
-            recorder.start().unwrap();
-        }
-
-        let frame = recorder.read().unwrap();
-
-        if let Some(result) = vosk::recognize(&frame, true) {
-            if result.is_empty() {
-                if !transcription.is_empty() {
-                    break;
-                }
-                continue;
-            }
-            if result == transcription {
-                continue;
-            }
-            app_handle
-                .emit_all("transcription", result.clone())
-                .expect("failed to emit transcription");
-            transcription = result;
-        }
-
-        sleep(Duration::from_millis(30));
+    let stream = match config.sample_format() {
+        SampleFormat::I8 => audio_input_device.build_input_stream(
+            &config.into(),
+            move |data: &[i8], _| recognize(app_handle.clone(), data, channels),
+            err_fn,
+            None,
+        ),
+        SampleFormat::I16 => audio_input_device.build_input_stream(
+            &config.into(),
+            move |data: &[i16], _| recognize(app_handle.clone(), data, channels),
+            err_fn,
+            None,
+        ),
+        SampleFormat::I32 => audio_input_device.build_input_stream(
+            &config.into(),
+            move |data: &[i32], _| recognize(app_handle.clone(), data, channels),
+            err_fn,
+            None,
+        ),
+        SampleFormat::F32 => audio_input_device.build_input_stream(
+            &config.into(),
+            move |data: &[f32], _| recognize(app_handle.clone(), data, channels),
+            err_fn,
+            None,
+        ),
+        sample_format => panic!("Unsupported sample format '{sample_format}'"),
     }
-    if recorder.is_recording() {
-        recorder.stop().unwrap();
-    }
-    transcription
+    .expect("Could not build stream");
+
+    stream.play().expect("Could not play stream");
+    sleep(Duration::from_secs(5));
+    drop(stream);
+    let result = "Transcription stopped".to_string();
+    result
 }
 
 fn main() {
-    vosk::init_vosk();
+    let audio_input_device = cpal::default_host()
+        .default_input_device()
+        .expect("No input device connected");
+
+    let config = audio_input_device
+        .default_input_config()
+        .expect("Failed to load default input config");
+    vosk::init_vosk(config.sample_rate().0 as f32);
     tauri::Builder::default()
       // This is where you pass in your commands
       .invoke_handler(tauri::generate_handler![listen_and_transcribe])
       .run(tauri::generate_context!())
       .expect("failed to run app");
+}
+
+
+fn recognize<T: Sample + ToSample<i16>>(
+    app_handle: tauri::AppHandle,
+    data: &[T],
+    channels: ChannelCount,
+)  {
+    let data: Vec<i16> = data.iter().map(|v| v.to_sample()).collect();
+    let data = if channels != 1 {
+        stereo_to_mono(&data)
+    } else {
+        data
+    };
+    let recognized = vosk::recognize(&data, true);
+    if let Some(text) = recognized {
+        if text.is_empty() {
+            return;
+        }
+        app_handle.emit_all("transcription", text.clone()).expect("failed to emit transcription");
+        println!("Recognized: {}", text);
+    }
+}
+
+pub fn stereo_to_mono(input_data: &[i16]) -> Vec<i16> {
+    let mut result = Vec::with_capacity(input_data.len() / 2);
+    result.extend(
+        input_data
+            .chunks_exact(2)
+            .map(|chunk| chunk[0] / 2 + chunk[1] / 2),
+    );
+
+    result
 }
