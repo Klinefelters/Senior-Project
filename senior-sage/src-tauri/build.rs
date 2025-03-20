973d03964fe8a9d6c05c8565f3e8d03b86a74995
Josh Schmitt <jvschmitt.ola@gmail.com>
2025-03-20 13:42:56 -0400

    Added a conditional compile for linux

--- senior-sage/src-tauri/build.rs
+++ senior-sage/src-tauri/build.rs
@@ -1,5 +1,10 @@
 fn main() {
-    let lib_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap()).join("/home/orangepi/Senior-Project/senior-sage/src-tauri/libvosk");
-    println!("cargo:rustc-link-search=libvosk");
-    tauri_build::build()
+    #[cfg(all(target_os = "linux", target_arch = "arm"))]
+    {
+        let lib_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap())
+            .join("/home/orangepi/Senior-Project/senior-sage/src-tauri/libvosk");
+        println!("cargo:rustc-link-search=libvosk");
+    }
+
+    tauri_build::build();
 }
