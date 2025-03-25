0fd1fdf997e3c0f000620a05eb2edb86fa0c39b6
joshschmitt35 <157548551+joshschmitt35@users.noreply.github.com>
2025-03-25 12:43:50 -0400

    build.rs change

--- senior-sage/src-tauri/build.rs
+++ senior-sage/src-tauri/build.rs
@@ -1,5 +1,5 @@
 fn main() {
-    #[cfg(all(target_os = "linux", target_arch = "arm"))]
+    #[cfg(all(target_os = "linux", target_arch = "aarch64"))]
     {
         let lib_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap())
             .join("/home/orangepi/Senior-Project/senior-sage/src-tauri/libvosk");
