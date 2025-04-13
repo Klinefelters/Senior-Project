507821493490ca01f28aa884b7e64b51d5aa93e6
joshschmitt35 <157548551+joshschmitt35@users.noreply.github.com>
2025-04-13 15:32:32 -0400

    Modified build.rs

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,10 +1,4 @@
 fn main() {
-    #[cfg(all(target_os = "linux", target_arch = "aarch64"))]
-    {
-        let lib_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap())
-            .join("/home/orangepi/Senior-Project/senior-sage/src-tauri/libvosk");
         println!("cargo:rustc-link-search=libvosk");
-    }
-
-    tauri_build::build();
+        tauri_build::build();
 }
